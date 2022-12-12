fc15db7bd5b069747b5a4e3c8b2fb21112a39605
ALESSIO CARACHINO s296138 <alessio.carachino.g@gmail.com>
2022-12-12 13:28:58 +0100

    added RL against optimal

--- lab3/task4_lib.py
+++ lab3/task4_lib.py
@@ -13,7 +13,7 @@ NIM_SIZE = 5
 def task4_run(params):
     won = 0
     nim = Nim(NIM_SIZE, k=None)
-    max_wr = (0,-1)
+    max_wr = (0, -1)
     moveHistory = []
     indices = []
     robot = Agent(nim, alpha=params['alpha'], random_factor=params['random_factor'])
@@ -52,21 +52,22 @@ def task4_run(params):
 
     plt.ylabel('winrate %')
     plt.xlabel('# games')
-    plt.ylim(0,100)
+    plt.ylim(0, 100)
     plt.plot(indices, moveHistory, "b")
     plt.show()
+
 
 def task4_Q(hyperparams):
     won = 0
     status = None
     nim = Nim(NIM_SIZE, k=None)
-    max_wr = (0,-1)
+    max_wr = (0, -1)
     moveHistory = []
     indices = []
     q_agent = Q_agent(nim, hyperparams)
     for m in tqdm(range(NUM_MATCHES)):
         player = 0
-        #q_agent = Q_agent(nim, hyperparams)
+        # q_agent = Q_agent(nim, hyperparams)
         while nim:
             if player == 0:
                 action = q_agent.Q_move(nim)
@@ -74,6 +75,55 @@ def task4_Q(hyperparams):
                 q_agent.Q_post(nim, status)
             else:
                 ply = pure_random(nim)
+                nim.nimming(ply)
+
+            player = 1 - player
+        if player == 1:
+            won += 1
+            status = 'win'
+        else:
+            status = 'lose'
+        q_agent.Q_post(nim, status=status)
+        status = None
+        # get a history of number of steps taken to plot later
+        if m % 100 == 0:
+            if m == 0:
+                continue
+            # print(f"{m}: {won}/{100}")
+            winrate = won / 100 * 100
+            moveHistory.append(winrate)
+            indices.append(m)
+            won = 0
+            if max_wr[0] < winrate:
+                max_wr = (winrate, m)
+        nim = Nim(NIM_SIZE, k=None)
+
+    print('max winrate: ', max_wr)
+    plt.ylabel('winrate %')
+    plt.xlabel('# games')
+    plt.ylim(0, 100)
+    plt.plot(indices, moveHistory, "b")
+    plt.show()
+
+
+def task4_Q_optimal(hyperparams, iterations: int):
+    won = 0
+    status = None
+    nim = Nim(NIM_SIZE, k=None)
+    max_wr = (0, -1)
+    moveHistory = []
+    indices = []
+    q_agent = Q_agent(nim, hyperparams)
+    for m in tqdm(range(iterations)):
+        player = 0
+        # q_agent = Q_agent(nim, hyperparams)
+        while nim:
+            if player == 0:
+                action = q_agent.Q_move(nim)
+                nim.nimming(action)
+                q_agent.Q_post(nim, status)
+            else:
+                ply = optimal_strategy(nim)
                 nim.nimming(ply)
 
             player = 1 - player
@@ -100,54 +150,6 @@ def task4_Q(hyperparams):
     print('max winrate: ', max_wr)
     plt.ylabel('winrate %')
     plt.xlabel('# games')
-    plt.ylim(0,100)
-    plt.plot(indices, moveHistory, "b")
-    plt.show()
-
-def task4_Q_optimal(hyperparams, iterations: int):
-    won = 0
-    status = None
-    nim = Nim(NIM_SIZE, k=None)
-    max_wr = (0,-1)
-    moveHistory = []
-    indices = []
-    q_agent = Q_agent(nim, hyperparams)
-    for m in range(iterations):
-        player = 0
-        #q_agent = Q_agent(nim, hyperparams)
-        while nim:
-            if player == 0:
-                action = q_agent.Q_move(nim)
-                nim.nimming(action)
-                q_agent.Q_post(nim, status)
-            else:
-                ply = optimal_strategy(nim)
-                nim.nimming(ply)
-
-            player = 1 - player
-        if player == 1:
-            won += 1
-            status = 'win'
-        else:
-            status = 'lose'
-        q_agent.Q_post(nim, status=status)
-        status = None
-        # get a history of number of steps taken to plot later
-        if m % 100 == 0:
-            if m == 0:
-                continue
-            print(f"{m}: {won}/{100}")
-            winrate = won / 100 * 100
-            moveHistory.append(winrate)
-            indices.append(m)
-            won = 0
-            if max_wr[0] < winrate:
-                max_wr = (winrate, m)
-        nim = Nim(NIM_SIZE, k=None)
-
-    print('max winrate: ', max_wr)
-    plt.ylabel('winrate %')
-    plt.xlabel('# games')
-    plt.ylim(0,100)
+    plt.ylim(0, 100)
     plt.plot(indices, moveHistory, "b")
     plt.show()
